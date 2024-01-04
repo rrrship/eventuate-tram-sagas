a1a4da87e8ce6054830288298eb67b5cabac7370
Klaus Vahter <klaus.vahter@concise.ee>
2024-01-04 11:11:34 +0200

    example #1, fix serialization issue

--- eventuate-tram-sagas-reactive-orchestration-simple-dsl/src/test/java/io/eventuate/tram/sagas/reactive/simpledsl/example/SagaData.java
+++ eventuate-tram-sagas-reactive-orchestration-simple-dsl/src/test/java/io/eventuate/tram/sagas/reactive/simpledsl/example/SagaData.java
@@ -2,4 +2,15 @@ package io.eventuate.tram.sagas.reactive.simpledsl.example;
 
 public class SagaData {
     String failureReason;
+
+    public SagaData() {
+    }
+
+    public String getFailureReason() {
+        return failureReason;
+    }
+
+    public void setFailureReason(String failureReason) {
+        this.failureReason = failureReason;
+    }
 }
